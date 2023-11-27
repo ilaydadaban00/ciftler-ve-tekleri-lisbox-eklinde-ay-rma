eadcb02c9eab9adeef0c491c0aabc86c46f1760a
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:08:18 +0300

    Add files via upload

---
+++ ciftleri bi tarafa tekleri bitarafa alan program/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ciftleri_bi_tarafa_tekleri_bitarafa_alan_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i <= 20; i++)
+            {
+                listBox1.Items.Add(rnd.Next(i));
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i <= 20; i++)
+            {
+                if(i%2 == 0)
+                {
+                    listBox2.Items.Add(i);
+                }
+                else
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+    }
+}
